3e285014522cfac4d9dcace22e4054e21dc72f9e
Veikko Nieminen <veikko.nieminen@gmail.com>
2020-12-18 15:30:04 +0200

    First components

---
+++ src/components/box.js
@@ -0,0 +1,17 @@
+import Button from 'react-bootstrap/Button';
+
+function Box() {
+    return (
+        <div class="box">
+            <div class="box-header">
+                Header
+            </div>
+            <div class="box-body">
+                Body
+                <Button>Vsad</Button>
+            </div>
+        </div>
+    )
+}
+
+export default Box;
