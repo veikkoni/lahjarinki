a2f4e2959773a6976d65a5ffd30dd2fba8fc7825
Veikko Nieminen <veikko.nieminen@tuni.fi>
2021-11-18 22:04:10 +0200

    Frontend wish functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [name, setName] = useState("");
+  const [wish, setWish] = useState("");
+  const [unwish, setUnwish] = useState("");
+  const [participating, setParticipating] = useState();
+  const circle = "1"
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    fetch('http://localhost:5000/join', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        wish: wish,
+        unwish: unwish,
+        participating: participating,
+        circle: circle,
+      })
+    }).catch((err) => {
+      console.log(err);
+    });
+    
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form>
+      <label>Enter your name:
+        <input
+          type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <label>Enter your name:
+        <input
+          type="text" 
+          value={wish}
+          onChange={(e) => setWish(e.target.value)}
+        />
+      </label>
+      <label>Enter your name:
+        <input
+          type="text" 
+          value={unwish}
+          onChange={(e) => setUnwish(e.target.value)}
+        />
+      </label>
+      <label>Osallistutko
+        <input
+          type="radio"
+          value="yes"
+          name="participating"
+          value={participating}
+          onChange={(e) => setParticipating(true)}
+        />Kyllä
+          <input
+          type="radio"
+          value="no"
+          name="participating"
+          value={participating}
+          onChange={(e) => setParticipating(false)}
+        />Ei
+      </label>
+      <button type="submit" onClick={handleSubmit}>Lähetä</button>
+    </form>
+  )
+  
 }
 
 export default App;
