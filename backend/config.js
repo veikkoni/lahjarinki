6ceef792474aa7cb4d81fec5747b6bb18253e8e3
Veikko Nieminen <veikko.nieminen@tuni.fi>
2021-11-19 22:30:24 +0200

    Compose backend port fix, cors fix

--- backend/config.js
+++ backend/config.js
@@ -1,8 +1,10 @@
 
 const mongoUrl = process.env.MONGODB_URL;
 const port = process.env.API_PORT || 3000;
+const clientAddress = process.env.CLIENT_ADDRESS;
 
 module.exports = {
     mongoUrl: mongoUrl,
-    port: port
+    port: port,
+    clientAddress: clientAddress
 };
